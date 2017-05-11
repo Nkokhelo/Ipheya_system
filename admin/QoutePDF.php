7da1a68e4e14d0c7873e2c24f9a1e79d0c934716
Programmer <Programmer>
2017-05-11 17:57:55 +0200

    21406993

--- admin/QoutePDF.php
+++ admin/QoutePDF.php
@@ -1,10 +1,12 @@
 <?php
  include '../core/Libaries/fpdf.php';
  $pdf = new FPDF();
-
+ 
  $pdf->AddPage();
- $pdf->SetFont("Arial","B",30);
- $pdf->Cell(0,10,"Nhlakanipho's First PDF",1);
-
+ $pdf->SetFont("Arial","B",20);
+ $pdf->Cell($pdf->GetPageWidth()/2,10,"Ipheya IT Solution",0,1,'L');
+ $pdf->Cell($pdf->GetPageWidth()/2,10,"Ipheya IT Solution",0,1,'R');
+ $pdf->SetFont("Arial","",13);
+ $pdf->Cell(0,10,"Nhlakanipho's First PDF",0,2);
  $pdf->Output();
 ?>
