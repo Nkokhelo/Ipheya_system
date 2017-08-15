141579b574e2f66c6230b626d4f9164409c7fb5f
Transcendence17 <transcendence2017@gmail.com>
2017-08-15 17:19:05 +0200

    21406993

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -1,5 +1,4 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" id="navbar-main">
-<<<<<<< HEAD
   <div class="container-fluid">
      <div class="navbar-header">
           <button type="button" class="navbar-toggle" style="color:#fff;" data-toggle="collapse" data-target="#main-menu">
@@ -63,57 +62,6 @@
             </ul>
         </div>
       </div>
-=======
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" style="color:#fff;" data-toggle="collapse" data-target="#main-menu">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a   href="index.php" class="navbar-brand text-uppercase">Ipheya</a>
-  </div>
-  <div class="container collapse navbar-collapse" id="main-menu">
-    <ul class="nav navbar-nav">
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="">Departments<span class="caret"></span></a>
-        <ul class="dropdown-menu" role="menu">
-          <li><a  href="departments.php">Departments</a></li>
-          <li><a  href="services.php">Services</a></li>
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="">Employees<span class="caret"></span></a>
-        <ul class="dropdown-menu" role="menu">
-            <li><a  href="employees.php">Employees</a></li>
-            <li><a  href="roles.php">Roles</a></li>
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="">Clients<span class="caret"></span></a>
-        <ul class="dropdown-menu" role="menu">
-          <li><a  href="clients.php">All clients</a></li>
-          <li><a  href="archives.php">Archived accounts</a></li>
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="">Client Request<span class="caret"></span></a>
-        <ul class="dropdown-menu" role="menu">
-          <li><a  href="clientRequest.php">Requests</a></li>
-          <li><a href="Tickets.php">Tickets</a></li>
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-target="">Survey<span class="caret"></span></a>
-        <ul class="dropdown-menu" role="menu">
-          <li><a href="../technician/CreateSurvey.php">Record Survey</a></li>
-          <li><a href="../technician/View.php">View survey</a></li>
-        </ul>
-      </li>
-      <li><a href="CreateTask.php">Create Task</a></li>
-      <li><a href="../inventory/items.php">Inventory</a></li>
-      <li><a href="targets.php">Targets</a></li>
-    </ul>
->>>>>>> 3e1b0c896d9d12b3f76cfc6406b6bd3a9c50292a
   </div>
 </nav>
 <body id="admin-dashboard">
