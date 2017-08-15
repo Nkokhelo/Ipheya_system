6210ae7ed9ed894c58cef5de0b5dc3355e312404
Transcendence17 <transcendence2017@gmail.com>
2017-08-15 14:56:39 +0200

    21401824

--- manager/allexpence.php
+++ manager/allexpence.php
@@ -125,10 +125,7 @@
                                 </div> 
                                 <label class="col-xs-2 control-label" for="amount">Amount :</label>
                                 <div class="col-xs-4">
-                                    <select class="form-control" name="ei_amount">
-                                        <option style="background:#aaa">--None--</option>
-                                        <?=$categories_dd?>
-                                    </select>
+                                   <input type="text" name="amount" value="" id="amount">
                                 </div> 
                               </div>
                                 <hr style="width:100%"/>  
@@ -193,6 +190,21 @@
            dateFormat: 'yy-mm-dd'
           }
         );
+         $("#amount").keydown(function (e) {
+              // Allow: backspace, delete, tab, escape, enter and .
+              if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+                  // Allow: Ctrl+A, Command+A
+                  (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+                  // Allow: home, end, left, right, down, up
+                  (e.keyCode >= 35 && e.keyCode <= 40)) {
+                      // let it happen, don't do anything
+                      return;
+              }
+              // Ensure that it is a number and stop the keypress
+              if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                  e.preventDefault();
+              }
+        });
         $('#yes').click(function(){
           $('#files').show();
         });
@@ -218,10 +230,6 @@
             }
           });
 
-          $('form').submit(function(evt){
-            alert('stop');
-            evt.preventDefault();// to stop form submitting
-          });
           //form validator file 
           $('INPUT[type="file"]').change(function () {
             var ext = this.value.match(/\.(.+)$/)[1];
