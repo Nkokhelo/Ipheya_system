6b4aa48de56905231b2c35d709cf089695453efc
Transcendence17 <transcendence2017@gmail.com>
2017-08-15 08:41:24 +0200

    mfundo

--- core/controllers/expenses-controller.php
+++ core/controllers/expenses-controller.php
@@ -1,7 +1,6 @@
 <?php
 
     $logic = new Logic();
-<<<<<<< HEAD
     if(isset($_POST['save']))
     {
         
@@ -11,13 +10,6 @@
     $payments =array();
     $project_dd ='';
     $categories_dd='';
-=======
-
-    $trans_query = "SELECT * FROM expenses";
-    $transactions='';
-    $payments =array();
-
->>>>>>> b7201f99d71a057ccf7d40f9bbc90ed5be45eafe
     $query = mysqli_query($logic->connect(),$trans_query);
     if(!$query)
     {
@@ -27,7 +19,6 @@
     {
         $transactions.="<tr><td>".$p['Date']."</td><td>".$p['payment_status']."</td><td>".$p['Amount_Paid']."</td><td style='color:green' align='center'><span class='glyphicon glyphicon-arrow-up'></span></td></tr>";
     }
-<<<<<<< HEAD
     $allproject =$logic->getallProjets();
     while($projects = mysqli_fetch_assoc($allproject))
     {
@@ -38,12 +29,5 @@
     while($categories = mysqli_fetch_assoc($allproject))
     {
       $categories_dd .= "<option value='".$categories['id']."'>".$categories["expense_name"]."</option>";
-=======
-    $project_dd='';
-    $allproject =$logic->getallProjets();
-    while($projects = mysqli_fetch_assoc($allproject))
-    {
-      $project_dd = "<option>".$project["name"]."</option>";
->>>>>>> b7201f99d71a057ccf7d40f9bbc90ed5be45eafe
     }
 ?>
