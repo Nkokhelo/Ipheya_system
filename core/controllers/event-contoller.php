5eae38566947386b27dc00355f3ddd665ae9ddf5
21432335 <mvelisojita@gmail.com>
2017-08-23 12:40:49 +0200

    21432335

--- core/controllers/event-contoller.php
+++ core/controllers/event-contoller.php
@@ -3,11 +3,11 @@
     $logic = new Logic();
    if(isset($_POST['Create_Event']))
     {
-        $name=$_POST['name'];
-        $description =$_POST['description'];
-        $category =$_POST['category'];
-        $date =$_POST['date'];  
-        $duration =$_POST['duration'];  
+        $name= $_POST['name'];
+        $description= $_POST['description'];
+        $category= $_POST['category'];
+        $date= $_POST['date'];  
+        $duration= $_POST['duration'];  
         
         $save = "INSERT INTO `events`(`name`,`description`,`category`,`date`,`duration`)      
                  VALUES (null,'$name','$description','$category','$date','$duration')";
