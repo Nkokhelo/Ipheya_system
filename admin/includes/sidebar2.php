02a97b6b54f603ece30c070d4acb4385020b1dbb
21539288 <alomac17@gmail.com>
2017-09-28 15:30:26 +0200

    21539288

---
+++ admin/includes/sidebar2.php
@@ -0,0 +1,90 @@
+<!-- Sidebar Holder -->
+<nav id="sidebar">
+<div class="sidebar-header">
+    <h3 style='font-weight:900'>IPHEYA</h3>
+</div>
+<!-- -My new side bar-->
+<ul class="list-unstyled components">
+  <li>
+    <a href="dashboard.php" >
+      <i class="fa fa-dashboard"></i>
+      Dashboard
+    </a>
+  <li>
+    <a href="#companyMenu" class="dropdown-toggle" data-toggle="collapse" aria-expanded="false">
+      <i class='fa fa-building-o'></i>
+      Departments
+    </a>
+      <ul class="collapse list-unstyled" id="companyMenu">
+        <li><a id="ddepa" href="departments.php">Departments</a></li>
+        <li><a id="dsup" href="allservices.php">Services</a></li>
+    </ul>
+  </li>
+  <li  >
+    <a href="#stakeholders" data-toggle="collapse" aria-expanded="false">
+      <i class='fa fa-vcard-o'></i>
+      HR Management
+    </a>
+    <ul class="collapse list-unstyled" id="stakeholders">
+        <li><a  href="allemployees.php">Employees</a></li>
+        <li><a  href="allsuppliers.php">Suppliers</a></li>
+        <li><a id="Eve" href="create_events.php">Events</a></li>
+        <li><a  href="roles.php">Roles</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="#clientMenu" data-toggle="collapse" aria-expanded="false">
+      <i class='fa fa-group'/></i>
+      Clients
+    </a>
+    <ul class="collapse list-unstyled" id="clientMenu">
+      <li><a  href="clients.php">All clients</a></li>
+      <li><a  href="archives.php">Archived accounts</a></li>
+    </ul>
+  </li>
+  <li  >
+    <a href="#requestsMenu" data-toggle="collapse" aria-expanded="false">
+        <i class='fa fa-copy'></i>
+      Request & Tickets
+    </a>
+    <ul class="collapse list-unstyled" id="requestsMenu">
+      <li><a  href="clientRequest.php">Requests</a></li>
+      <li><a href="Tickets.php">Tickets</a></li>
+      <li><a href="allfaqs.php">FAQ</a></li>
+    </ul>
+  </li>
+  <li>
+  <a href="#Inventory" data-toggle="collapse" aria-expanded="false">
+      <i class='fa fa-cubes'></i>
+      Stock
+    </a>
+    <ul class="collapse list-unstyled" id="Inventory">
+      <li><a  href="brand.php">Brand</a></li>
+      <li><a  href="category.php">Category</a></li>
+      <li><a  href="manageproducts.php">Products</a></li>
+      <li><a href="orders.php">Orders</a></li>
+      <li><a href="inventories.php">Inventory</a></li>
+      <li><a href="sales.php">Sales</a></li>
+      <li><a href="rentals.php">Rentals</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="alltasks.php">
+      <i class='fa fa-task'></i>
+      Task
+    </a>
+  </li>
+  <li>
+    <a href="targets.php">
+        <i class='fa fa-bullseye'></i>
+      Targets
+    </a>
+  </li>
+  <li>
+    <a href="../login.php">
+      <i class="fa fa-sign-out"></i>
+      Logout
+    </a>
+  </li>
+</ul>
+</nav>
--- core/MobileRequests/geolocation.js
+++ core/MobileRequests/geolocation.js
@@ -1,10 +1,24 @@
-$(function(){
-  navigator.geolocation.getCurrentPosition(success, error);
+$(document).ready(function(){
+  var interval = setInterval(function () {
+      $(function(){
+        navigator.geolocation.getCurrentPosition(success, error);
 
-  function success(position){
-    //getting the coordinates
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-
-    
+        function success(position){
+          //getting the coordinates
+          var lat = position.coords.latitude;
+          var long = position.coords.longitude;
+            $.ajax({
+                      type: "POST",
+                      url: "insert.php",
+                      data: "latitude=" + lat+ "&longitude=" + long,
+                      success: function(data) {
+                         alert("WTF");
+                      }
+                  });
+        }
+        function error(){
+          $('body').html("Error");
+        }
+      });
+  }, 1000);
 });
